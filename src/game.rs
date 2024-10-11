f26563d1b07204f739bab6a62f100a00612fe239
Robsutar <robsonsutar@gmail.com>
2024-10-11 05:02:46 -0300

    Define inner Scene components constructors

--- src/game.rs
+++ src/game.rs
@@ -14,6 +14,11 @@ struct GridPos {
     y: i32,
 }
 
+impl GridPos {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
 
 #[derive(Bundle)]
 struct SnakeHead {
@@ -21,6 +26,24 @@ struct SnakeHead {
     mesh: MaterialMesh2dBundle<ColorMaterial>,
 }
 
+impl SnakeHead {
+    fn new(pos: GridPos, assets: &GlobalAssets) -> Self {
+        let (mesh, material) = assets.snake_head_mesh_material.clone();
+        Self {
+            pos,
+            mesh: MaterialMesh2dBundle {
+                mesh,
+                material,
+                transform: Transform::from_xyz(
+                    RECT_SIZE * pos.x as f32,
+                    RECT_SIZE * pos.y as f32,
+                    0.0,
+                ),
+                ..default()
+            },
+        }
+    }
+}
 
 #[derive(Component)]
 enum ColliderVariant {
@@ -36,6 +59,33 @@ struct Collider {
     mesh: MaterialMesh2dBundle<ColorMaterial>,
 }
 
+impl Collider {
+    fn from_variant(variant: ColliderVariant, pos: GridPos, assets: &GlobalAssets) -> Self {
+        let ((mesh, material), variant) = match &variant {
+            ColliderVariant::Apple => (assets.apple_mesh_material.clone(), ColliderVariant::Apple),
+            ColliderVariant::Wall => (assets.wall_mesh_material.clone(), ColliderVariant::Wall),
+            ColliderVariant::SnakeBody => (
+                assets.snake_body_mesh_material.clone(),
+                ColliderVariant::SnakeBody,
+            ),
+        };
+
+        Self {
+            variant,
+            pos,
+            mesh: MaterialMesh2dBundle {
+                mesh,
+                material,
+                transform: Transform::from_xyz(
+                    RECT_SIZE * pos.x as f32,
+                    RECT_SIZE * pos.y as f32,
+                    0.0,
+                ),
+                ..default()
+            },
+        }
+    }
+}
 
 #[derive(Component)]
 pub struct Scene {
