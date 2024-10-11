c3904e8a4b967be33bc2a8bb8ab00ac362fab937
Robsutar <robsonsutar@gmail.com>
2024-10-11 01:22:00 -0300

    Initializes a simple egui example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
 fn main() {
-    println!("Hello World!")
+    App::default()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_systems(Startup, init)
+        .add_systems(Update, update)
+        .run();
+}
+
+fn init(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn update(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
 }
