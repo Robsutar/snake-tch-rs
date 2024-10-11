816ef7c0f5ef16560b91860e8b442b47cf5ef888
Robsutar <robsonsutar@gmail.com>
2024-10-11 05:01:26 -0300

    Create Scene inner components

--- src/game.rs
+++ src/game.rs
@@ -1,1 +1,37 @@
+use std::collections::HashMap;
 
+use crate::RECT_SIZE;
+
+use super::GlobalAssets;
+use bevy::{
+    prelude::*,
+    sprite::{ColorMaterial, MaterialMesh2dBundle},
+};
+
+#[derive(Component, Debug, Eq, PartialEq, Hash, Clone, Copy)]
+struct GridPos {
+    x: i32,
+    y: i32,
+}
+
+
+#[derive(Bundle)]
+struct SnakeHead {
+    pos: GridPos,
+    mesh: MaterialMesh2dBundle<ColorMaterial>,
+}
+
+
+#[derive(Component)]
+enum ColliderVariant {
+    Apple,
+    Wall,
+    SnakeBody,
+}
+
+#[derive(Bundle)]
+struct Collider {
+    variant: ColliderVariant,
+    pos: GridPos,
+    mesh: MaterialMesh2dBundle<ColorMaterial>,
+}
