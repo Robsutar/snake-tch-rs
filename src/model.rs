6f0d048d7d84b7e142c04266cd028ab6910f053d
Robsutar <robsonsutar@gmail.com>
2024-10-12 12:44:36 -0300

    Create QTrainer struct

--- src/model.rs
+++ src/model.rs
@@ -69,3 +69,85 @@ impl Module for LinerQNet {
     }
 }
 
+pub struct QTrainer {
+    pub model: LinerQNet,
+    gamma: f32,
+    optimizer: Optimizer,
+}
+
+impl QTrainer {
+    pub fn new(vs: &VarStore, model: LinerQNet, lr: f64, gamma: f32) -> Self {
+        let optimizer = Adam::default().build(&vs, lr).unwrap();
+        Self {
+            model,
+            gamma,
+            optimizer,
+        }
+    }
+
+    fn tensor_train_step(
+        &mut self,
+        state: Tensor,
+        action: Tensor,
+        reward: Tensor,
+        next_state: Tensor,
+        done: Vec<bool>,
+    ) {
+        let pred = self.model.forward(&state);
+
+        let mut target = pred.copy();
+        for idx in 0..done.len() as i64 {
+            let mut q_new = reward.i(idx);
+            if !done[idx as usize] {
+                q_new = q_new + self.gamma * self.model.forward(&next_state.i(idx)).max();
+            }
+
+            let _ = target.index_put_(
+                &[
+                    Some(Tensor::from_slice(&[idx])),
+                    Some(Tensor::from_slice(&[action
+                        .i(idx)
+                        .argmax(0, false)
+                        .int64_value(&[])])),
+                ],
+                &q_new,
+                false,
+            );
+        }
+
+        self.optimizer.zero_grad();
+        let loss = target.mse_loss(&pred, tch::Reduction::Mean);
+        loss.backward();
+
+        self.optimizer.step();
+    }
+
+    pub fn train_multiple_steps(&mut self, snapshots: SnapshotConcat) {
+        if !snapshots.is_built() {
+            panic!(
+                "SnapshotConcat is not built, filled: {:?}, target size: {:?}",
+                snapshots.done.len(),
+                snapshots.target_size
+            );
+        }
+        let len = snapshots.target_size as i64;
+        let state_view = [len, STATE_SIZE as i64];
+        let next_state_view = [len, STATE_SIZE as i64];
+        let action_view = [len, ACTION_SIZE as i64];
+        let reward_view = [len, 1];
+
+        let state = Tensor::from_slice(&snapshots.state).view(state_view);
+        let next_state = Tensor::from_slice(&snapshots.next_state).view(next_state_view);
+        let action = Tensor::from_slice(&snapshots.action).view(action_view);
+
+        let reward = Tensor::from_slice(&snapshots.reward).view(reward_view);
+
+        self.tensor_train_step(state, action, reward, next_state, snapshots.done);
+    }
+
+    pub fn train_single_step(&mut self, snapshot: &Snapshot) {
+        let mut snapshots = SnapshotConcat::building(1);
+        snapshots.push(snapshot);
+        self.train_multiple_steps(snapshots);
+    }
+}
