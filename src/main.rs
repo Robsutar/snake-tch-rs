5527da2a02749f26a8b07072bd73c326a8aaa2f6
Robsutar <robsonsutar@gmail.com>
2024-10-13 03:13:49 -0300

    Add IA controller/training systems

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ mod game;
 mod model;
 mod utils;
 
+use std::sync::Mutex;
+
+use agent::Agent;
 use bevy::{prelude::*, sprite::Mesh2dHandle};
 use bevy_egui::{
     egui::{self, Id},
     EguiContexts, EguiPlugin,
 };
 use game::{
-    init_scene, AppleMarker, ColliderMarker, Scene, SnakeHead, SnakeHeadMarker, SnakeOrientation,
+    init_scene, AppleMarker, ColliderMarker, PlayerStepAction, PlayerStepResult, Scene,
+    SnakeHeadMarker, SnakeOrientation,
 };
+use model::{Snapshot, ACTION_SIZE};
 use tch::Device;
 
 pub const RECT_SIZE: f32 = 10.0;
@@ -21,6 +26,7 @@ pub const ARENA: IRect = IRect {
 };
 
 pub type MaterialMesh = (Mesh2dHandle, Handle<ColorMaterial>);
+pub type DType = f32;
 
 #[derive(Resource)]
 pub struct GlobalAssets {
@@ -56,10 +62,32 @@ impl HumanController {
     }
 }
 
+#[derive(Component)]
+struct AiController {
+    plot_scores: Vec<usize>,
+    plot_mean_scores: Vec<f64>,
+    total_score: usize,
+    record: usize,
+    agent: Mutex<Agent>,
+}
+impl AiController {
+    fn action(raw: &[DType; ACTION_SIZE]) -> PlayerStepAction {
+        if raw[0] == 1.0 {
+            PlayerStepAction::Forward
+        } else if raw[1] == 1.0 {
+            PlayerStepAction::Left
+        } else {
+            PlayerStepAction::Right
+        }
+    }
+}
+
 fn main() {
     App::default()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
+        .add_systems(Startup, (init_assets, init_ai).chain())
+        .add_systems(Update, ai_update)
         .add_systems(Update, ui_info_update)
         .run();
 }
@@ -118,7 +146,7 @@ fn ui_info_update(
                     });
                 });
         }
-}
+    }
 }
 
 fn init_human(mut commands: Commands, assets: Res<GlobalAssets>) {
@@ -180,14 +208,128 @@ fn human_update(
                 }
                 PlayerStepResult::Collision => {
                     println!("Collision! Game reset");
-                scene.reset(
-                    &mut commands,
-                    &assets,
-                    &mut snake_head_transform,
-                    &mut apple_transform,
-                );
+                    scene.reset(
+                        &mut commands,
+                        &assets,
+                        &mut snake_head_transform,
+                        &mut apple_transform,
+                    );
                 }
             }
+        }
+    }
+}
+
+fn init_ai(mut commands: Commands, assets: Res<GlobalAssets>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let scene_id = init_scene(&mut commands, &assets);
+    commands.entity(scene_id).insert(AiController {
+        plot_scores: Vec::new(),
+        plot_mean_scores: Vec::new(),
+        total_score: 0,
+        record: 0,
+        agent: Mutex::new(Agent::load_if_exists("model.ot")),
+    });
+}
+
+fn ai_update(
+    mut commands: Commands,
+    assets: Res<GlobalAssets>,
+    mut scene_query: Query<(&mut Scene, &mut AiController)>,
+    mut snake_head_query: Query<
+        &mut Transform,
+        (
+            With<SnakeHeadMarker>,
+            Without<AppleMarker>,
+            Without<ColliderMarker>,
+        ),
+    >,
+    mut apple_query: Query<
+        &mut Transform,
+        (
+            With<AppleMarker>,
+            Without<SnakeHeadMarker>,
+            Without<ColliderMarker>,
+        ),
+    >,
+    mut collider_query: Query<&mut Transform, With<ColliderMarker>>,
+) {
+    if true {
+        let (mut scene, mut controller) = scene_query.single_mut();
+        let mut snake_head_transform = snake_head_query.get_mut(scene.snake_head.ge.id).unwrap();
+        let mut apple_transform = apple_query.get_mut(scene.apple.ge.id).unwrap();
+
+        let mut agent = controller.agent.lock().unwrap();
+
+        let state_old = Agent::get_state(&scene);
+
+        let final_move = agent.get_action(&state_old);
+
+        let (reward, done, score) = match scene.play_step(
+            &mut commands,
+            &assets,
+            &mut collider_query,
+            &mut snake_head_transform,
+            &mut apple_transform,
+            AiController::action(&final_move),
+        ) {
+            PlayerStepResult::Nothing => {
+                if scene.frame_iteration > 100 * scene.snake_len() {
+                    (-10.0, true, scene.punctuation)
+                } else {
+                    (0.0, false, scene.punctuation)
+                }
+            }
+            PlayerStepResult::AppleEaten => (10.0, false, scene.punctuation),
+            PlayerStepResult::Collision => {
+                scene.frame_iteration += 1;
+                (-10.0, true, scene.punctuation)
+            }
+        };
+
+        let state_new = Agent::get_state(&scene);
+
+        let snapshot = Snapshot {
+            state: state_old,
+            action: final_move,
+            reward,
+            next_state: state_new,
+            done,
+        };
+
+        agent.train_short_memory(&snapshot);
+
+        agent.remember(snapshot);
+
+        if done {
+            scene.reset(
+                &mut commands,
+                &assets,
+                &mut snake_head_transform,
+                &mut apple_transform,
+            );
+            agent.n_games += 1;
+            agent.train_long_memory();
+
+            let n_games = agent.n_games;
+            if score > controller.record {
+                agent.save("model.ot").unwrap();
+                drop(agent);
+                controller.record = score;
+            } else {
+                drop(agent);
+            }
+
+            println!(
+                "Game: {:?}, Score: {:?}, Record: {:?}",
+                n_games, score, controller.record
+            );
+
+            controller.plot_scores.push(score);
+            controller.total_score += score;
+            let mean_score = controller.total_score as f64 / n_games as f64;
+            controller.plot_mean_scores.push(mean_score);
         }
     }
 }
